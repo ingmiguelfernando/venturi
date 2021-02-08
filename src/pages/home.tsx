1ded35557cd3ed2a051495ab576be02699106e59
ingmiguelfernando <ing.miguel.fernando@gmail.com>
2021-02-07 19:52:07 -0500

    Background image in home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,14 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
 import { useAppContext } from "../context";
 import { SearchAppBar } from "../components/SearchAppBar";
+import styled from "styled-components";
+
+const BackgroundImage = styled.div`
+  background: url(/airplane-background.png) center center no-repeat;
+  background-position: right -1000px bottom 0px;
+  background-size: auto 100%;
+  background-color: black;
+  filter: sepia(200%) hue-rotate(199deg) saturate(209%);
+`;
 
 const home = () => {
   const { user } = useAppContext();
@@ -9,10 +16,12 @@ const home = () => {
   if (!user) return null;
 
   return (
-    <>
+    <div className="bg-black">
       <SearchAppBar />
-      <div>this is home</div>
-    </>
+      <BackgroundImage className="h-screen w-screen overflow-hidden z-0">
+        <div className="text-white">this is home</div>
+      </BackgroundImage>
+    </div>
   );
 };
 
