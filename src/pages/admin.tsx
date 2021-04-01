bbbcb2e37146bb8b6037a8994381374a23ad9a48
ingmiguelfernando <ing.miguel.fernando@gmail.com>
2021-04-01 17:52:37 -0500

    Added a vertical tab for admin

---
+++ src/pages/admin.tsx
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import { SearchAppBar } from "../components/SearchAppBar";
+import Head from "next/head";
+import Tab from "@material-ui/core/Tab";
+import Tabs from "@material-ui/core/Tabs";
+import Box from "@material-ui/core/Box";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: any;
+  value: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      className="w-2/3"
+      {...other}
+      style={{ outlineStyle: "none" }}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </div>
+  );
+}
+
+function a11yProps(index: any) {
+  return {
+    id: `vertical-tab-${index}`,
+    "aria-controls": `vertical-tabpanel-${index}`,
+  };
+}
+
+function getTab(id: number, label: string) {
+  return (
+    <Tab
+      label={label}
+      {...a11yProps(id)}
+      className="h-16"
+      style={{ outlineStyle: "none" }}
+    />
+  );
+}
+
+export default function admin() {
+  const [selectedTab, setSelectedTab] = useState(0);
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setSelectedTab(newValue);
+  };
+
+  return (
+    <>
+      <SearchAppBar />
+      <div className="bg-black relative h-screen">
+        <Head>
+          <title>Venturi</title>
+          <link rel="icon" href="/venturi_logo.svg" />
+        </Head>
+
+        <Tabs
+          orientation="vertical"
+          variant="scrollable"
+          value={selectedTab}
+          onChange={handleChange}
+          aria-label="Vertical tabs example"
+          className="w-1/4 items-center text-white pt-12"
+          indicatorColor="primary"
+          TabIndicatorProps={{
+            style: {
+              width: "2px",
+              borderRight: `1px solid green`,
+            },
+          }}
+        >
+          {getTab(0, "Courses")}
+          {getTab(1, "Modules")}
+          {getTab(2, "Segments")}
+        </Tabs>
+        <TabPanel value={selectedTab} index={0}>
+          Item One
+        </TabPanel>
+        <TabPanel value={selectedTab} index={1}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={selectedTab} index={2}>
+          tres
+        </TabPanel>
+      </div>
+    </>
+  );
+}
